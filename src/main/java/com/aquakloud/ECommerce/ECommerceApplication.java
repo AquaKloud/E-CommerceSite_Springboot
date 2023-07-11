d884a7ca2cd70363f0d88113192b9e481113a815
Sanjeewa Yapa <aquakloud0829@gmail.com>
2023-07-11 11:53:00 +0530

    Create E commerce App

---
+++ src/main/java/com/aquakloud/ECommerce/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.aquakloud.ECommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
